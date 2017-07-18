49f7a6d138287e409c3825d7500796dae33df563
Sergio Guillen <serguimant@gmail.com>
2017-07-17 20:23:48 -0400

    Create ui application using create-react-app

---
+++ ui/src/containers/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="col-md-8 col-md-offset-2">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              <b>Simple Cluster Panel</b>
+            </div>
+            <div className="panel-body">
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
