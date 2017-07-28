21291d180ec3ba27dd69e21627ec12d421a3a6de
Sergio Guillen <serguimant@gmail.com>
2017-07-27 23:21:39 -0400

    Replace bootstrap and font-awesome using cdn

--- ui/src/containers/App.js
+++ ui/src/containers/App.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css'
 import EcsContainer from './EcsContainer';
 
 class App extends Component {
